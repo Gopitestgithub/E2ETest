bdefcb8ead8e0d021655e5f7f35cc735eacb5659
gopi <gopisat3@gmail.com>
2020-05-24 01:56:40 +0530

    Indian 2

--- src/main/java/pageObject/TestClass.java
+++ src/main/java/pageObject/TestClass.java
@@ -5,5 +5,11 @@ public class TestClass {
 	
 	public static void main(String args []) {
 		System.out.println("American guy");
+		
+		
+			System.out.println("Indian guy");
+		
+		
+			System.out.println("Indian guy 2");
 	}
 }
